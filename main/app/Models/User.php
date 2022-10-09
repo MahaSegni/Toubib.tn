62bb8da1149a139f7af23fbf781766eecd680e19
AmaraMed <mohamed.amara.1@esprit.tn>
2022-10-09 14:44:58 +0100

    centres_services

--- main/app/Models/User.php
+++ main/app/Models/User.php
@@ -11,7 +11,9 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    public function centre(){
+        return $this->belongsTo(Centre::class);
+    }
     /**
      * The attributes that are mass assignable.
      *
---
+++ main/database/migrations/2022_10_09_131338_create_centres_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('centres', function (Blueprint $table) {
+            $table->id();
+            $table->text('nom');
+            $table->text('gouvernorat');
+            $table->text('adresse');
+            $table->text('telephone');
+            $table->text('description');
+            $table->foreignId('user_id')
+                ->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('centres');
+    }
+};
