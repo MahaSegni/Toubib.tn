8bb25636faf45ea6f06a3aa841040b7affebb9b4
MaalawiMahdi <mehdi.maalawi@esprit.tn>
2022-10-24 22:32:45 +0100

    push migration

---
+++ main/database/migrations/2022_10_12_120000_create_categorie_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('categorie_articles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('libelle');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorie_articles');
+    }
+};
