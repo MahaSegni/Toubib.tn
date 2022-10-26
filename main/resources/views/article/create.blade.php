9ccfd8697c293405ccab89b47e63fe0a1e25db40
EmnaBesbes <emna.besbes@esprit.tn>
2022-10-26 10:16:41 +0100

    second push

--- main/resources/views/article/create.blade.php
+++ main/resources/views/article/create.blade.php
@@ -17,31 +17,41 @@
                 <form method="post" action="{{ route('article.store') }}" enctype="multipart/form-data">
                     @csrf
                     <div class="form-group">
-                        <label for="titre">Titre:</label>
-                        <input type="text" class="form-control" name="titre" />
-                        <label for="texte">Texte:</label>
-                        <input type="text" class="form-control" name="texte"  />
-                        <label for="auteur">Auteur:</label>
-                        <input type="text" class="form-control" name="auteur"  />
-                        <label for="categorie_article_id" >Categorie :</label>
+                        <div class="form-group">
+                            <label for="titre">Titre:</label>
+                            <input type="text" class="form-control" name="titre" />
+                        </div>
+                        <div class="form-group">
+                            <label for="texte">Texte:</label>
+                            <textarea type="text" class="form-control" name="texte" rows="5" ></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="auteur">Auteur:</label>
+                            <input type="text" class="form-control" name="auteur"  />
+                        </div>
+                        <div class="form-group">
+                            <label for="categorie_article_id" >Categorie :</label>
                             <select name="categorie_article_id"  class="form-control">
                                 @foreach($categoriesArticle as $cat)
                                     <option  value="{{ $cat->id }}">{{ $cat->libelle }} </option>
                                 @endforeach
                             </select>
-                        <label for="image">Image:</label>
-                        <input type="file" name="image" class="form-control" placeholder="image" id="imgInp">
-                        <img style="visibility:hidden"  id="prview" src=""  width=100 height=100 />
-
-                        <br>
-                        <label for="video">Video:</label>
-                        <input type="file" name="video" class="form-control" placeholder="video" id="vidInp">
-                        <video style="visibility:hidden"  id="prviewVid" controls width="200" src=""/>
-
+                        </div>
+                        <div class="form-group">
+                            <label for="image">Image:</label>
+                            <input type="file" name="image" class="form-control" placeholder="image" id="imgInp">
+                            <br>
+                            <img style="visibility:hidden"  id="prview" src=""  width=100 height=100 />
+                        </div>
+                        <div class="form-group">
+                            <label for="video">Video:</label>
+                            <input type="file" name="video" class="form-control" placeholder="video" id="vidInp">
+                            <br>
+                            <video style="visibility:hidden"  id="prviewVid" controls width="200" src=""/>
+                        </div>
                     </div>
                     <button type="submit" class="btn btn-primary">Ajouter</button>
                 </form>
-
         </div>
     </div>
 
--- main/resources/views/article/edit.blade.php
+++ main/resources/views/article/edit.blade.php
@@ -18,29 +18,38 @@
                 @method('PATCH')
                 @csrf
                 <div class="form-group">
-                    <label for="titre">Titre:</label>
-                    <input type="text" class="form-control" name="titre" value={{ $article->titre }} />
-                    <label for="texte">Texte:</label>
-                    <textarea type="text" class="form-control" name="texte" > {{ $article->texte }}</textarea>
-                    <label for="auteur">Auteur:</label>
-                    <input type="text" class="form-control" name="auteur" value={{ $article->auteur }} />
-
-                    <label for="image">Image:</label>
-                    <input type="file" name="image" class="form-control" placeholder="image" id="imgInp">
-                    @if($article->image)
-                        <img id="prview" src="{{asset('images/imagesArticle/'.$article->image )}}"  width=100 height=100 />
-                    @else
-                        <img id="prview" src=""  style="visibility:hidden"  width=100 height=100 />
-                    @endif
-
-                    <br>
+                    <div class="form-group">
+                        <label for="titre">Titre:</label>
+                        <input type="text" class="form-control" name="titre" value={{ $article->titre }} />
+                    </div>
+                    <div class="form-group">
+                        <label for="texte">Texte:</label>
+                        <textarea type="text" class="form-control" name="texte" rows="5"> {{ $article->texte }}</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="auteur">Auteur:</label>
+                        <input type="text" class="form-control" name="auteur" value={{ $article->auteur }} />
+                    </div>
+                    <div class="form-group">
+                        <label for="image">Image:</label>
+                        <input type="file" name="image" class="form-control" placeholder="image" id="imgInp">
+                        <br>
+                        @if($article->image)
+                            <img id="prview" src="{{asset('images/imagesArticle/'.$article->image )}}"  width=100 height=100 />
+                        @else
+                            <img id="prview" src=""  style="visibility:hidden"  width=100 height=100 />
+                        @endif
+                    </div>
+                    <div class="form-group">
                     <label for="video">Video:</label>
-                    <input type="file" name="video" class="form-control" placeholder="video" id="vidInp">
-                    @if($article->video)
-                        <video  id="prviewVid" controls width="200" src="{{asset('videos/videosArticle/'.$article->video )}}"/>
-                    @else
-                        <video id="prviewVid" src="" controls  style="visibility:hidden"  width=200 />
-                    @endif
+                        <input type="file" name="video" class="form-control" placeholder="video" id="vidInp">
+                            <br>
+                        @if($article->video)
+                            <video  id="prviewVid" controls width="200" src="{{asset('videos/videosArticle/'.$article->video )}}"/>
+                        @else
+                            <video id="prviewVid" src="" controls  style="visibility:hidden"  width=200 />
+                        @endif
+                    </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Modifier</button>
             </form>
